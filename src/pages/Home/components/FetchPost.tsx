68b8b601561db2cd410990af2421fa81dcb7c6fb
Alendroide <alendroideyt@gmail.com>
2024-12-11 21:09:50 -0500

    Images now working

--- src/pages/Home/components/FetchPost.tsx
+++ src/pages/Home/components/FetchPost.tsx
@@ -1,8 +1,9 @@
 //Importaciones
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { postSchema } from "../../../schemas/postSchema";
 import axios from "axios";
+import '../css/styles.css'
 //Interface
 interface Props {
     reloadTrigger : boolean
@@ -42,34 +43,52 @@ export default function FetchPost({reloadTrigger} : Props){
     return(
         <>
             {posts.map((post,index)=>(
-                <div key={index} className="w-50 bg-light mx-auto p-5 shadow rounded-5 my-5">
-                    <div className="d-flex align-items-center mb-3">
-                        {post.user?.img?
-                        <img
-                            style={{
-                                width : '40px',
-                                height : '40px'
-                            }}
-                            src=""
-                        />
-                        :
-                        <img
-                            style={{
-                                width : '40px',
-                                height : '40px'
-                            }}
-                            src="/vite.svg"
-                        />
-                        }
-                        
-                        <div className="mx-3">
-                            <b>@{post.user?.name}</b>
+                    <div key={index} className="w-50 bg-light mx-auto p-5 shadow rounded-5 my-5">
+                        <Link to={`/post/${post.id}`}>
+                        <div className="d-flex align-items-center mb-3">
+                            {post.user?.img?
+                            <img
+                                style={{
+                                    width : '40px',
+                                    height : '40px'
+                                }}
+                                src=""
+                            />
+                            :
+                            <img
+                                style={{
+                                    width : '40px',
+                                    height : '40px'
+                                }}
+                                src="/vite.svg"
+                            />
+                            }
+                            
+                            <div className="mx-3">
+                                <b>@{post.user?.name}</b>
+                            </div>
                         </div>
+                        <h6>{post.title}</h6>
+                        <p>{post.body}</p>
+                        {post.img &&
+                        <div
+                            className="rounded-5 my-4"
+                            style={{
+                                maxHeight : '60vh',
+                                overflow : 'hidden'
+                            }}
+                        >
+                            <img
+                                src={post.img}
+                                style={{
+                                    width : '100%',
+                                }}
+                            />
+                        </div>
+                        }
+                        <p className="">{post.created_at}</p>
+                        </Link>
                     </div>
-                    <h6>{post.title}</h6>
-                    <p>{post.body}</p>
-                    <p className="">{post.created_at}</p>
-                </div>
             ))}
         </>
     )
